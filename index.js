afc01999f62d4a9e5a68c918ea64df4175eb5c8a
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-23 18:58:04 -0500

    index js, root route

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, 'client', 'build', 'public');
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//   res.render('./client/public/index.html');
-// });
+app.get('/', (req, res) => {
+  res.render('./client/public/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
