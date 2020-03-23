09ee8810bed65313d6a9ced48fc5353e72a6f45c
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-23 18:42:40 -0500

    index js, express import

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, 'client', 'public');
 app.use(express.static(publicPath));
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
