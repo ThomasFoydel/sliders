e54249134f555ae01ae9a3a1bad2b82b84ea57b9
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-23 19:07:41 -0500

    path name, index js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, 'client', 'build', 'public');
-app.use(express.static(publicPath));
 
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(publicPath));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
