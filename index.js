89bffbe870c99f350e311c055f6f4f576181db55
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-23 19:23:27 -0500

    mern deployment set up

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, 'client', 'build', 'public');
+let publicPath;
 
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+//production mode
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static(publicPath));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendfile(path.join((__dirname = 'client/build/index.html')));
   });
 }
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
