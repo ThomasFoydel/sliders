6e960e67347654243c3ee1348b13da9746676320
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-23 19:03:46 -0500

    index js, root route

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, 'client', 'build', 'public');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-  res.render('./client/public/index.html');
-});
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
